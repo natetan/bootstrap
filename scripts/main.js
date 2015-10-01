b4b510c1d19d210096a4528bbf5b78e37105b585
Yulong Tan <swifties@uw.edu>
2015-10-01 00:05:37 -0700

    fixed the reverse function

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,30 @@
-function canDrive(name, age) {
-	if (age > 16) {
-		return name + " is able to drive."
-	} else {
-		return name + " cannot drive yet."
+// function canDrive(name, age) {
+// 	if (age > 16) {
+// 		return name + " is able to drive."
+// 	} else {
+// 		return name + " cannot drive yet."
+// 	}
+// }
+
+// alert(canDrive(prompt("Type your name here"), prompt("How old are you?")));
+
+// var text = prompt("Write something and I'll reverse it")
+// function reverse(text) {
+// 	var output = "";
+// 	for (var i = text.length(); i >= 0; i--) {
+// 		output += text.charAt(i);
+// 	}
+// 	return output;
+// }
+
+// alert(reverse(text));
+
+function reverse(text) {
+	var output = "";
+	for (var i = text.length - 1; i >= 0; i--){
+		output += text.charAt(i);
 	}
+	return output;
 }
 
-alert(canDrive(prompt("Type your name here"), prompt("How old are you?")));
+// alert(reverse(prompt("I'll reverse anything you type here")));
